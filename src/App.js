7f358f9f62e6085807bc99482ef8b3593018ade5
rahul <rahulchauhah50@gmail.com>
2023-05-01 16:31:03 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <Router>
+        <Routes>
+          <Route eexat path='/' element={<Login/>} ></Route>
+          <Route eexat path='/home' element={[<Header/>,<Home/>]} ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
